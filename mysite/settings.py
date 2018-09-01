478572d34f47878a8577b026d5e8ddf2bff0158f
DESKTOP-QO3JHAF\Alexander <va771@mail.ru>
2018-09-01 22:53:13 +0700

    News app added

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mainApp',
+    'news',
     'webexample',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainApp.urls')),
     path('webexample/', include('webexample.urls')),
+    path('news/', include('news.urls')),
 ]
