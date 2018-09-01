b17d67e5379824c70488f9782cfcf1408c4c9c49
DESKTOP-QO3JHAF\Alexander <va771@mail.ru>
2018-09-01 23:31:05 +0700

    Recently added items are listed below: 1. Detaled view for a piece of news

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from news.models import Articles
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20], template_name="news/posts.html")),
+    path('<pk>', DetailView.as_view(model=Articles, template_name="news/post.html")),
 ]
