9cd318c4ad06dbeeee81d86b10d6deb0a2d447ef
DESKTOP-QO3JHAF\Alexander <va771@mail.ru>
2018-09-01 21:50:41 +0700

    A "Contact" text added

--- mainApp/views.py
+++ mainApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values':[ 'Если у Вас остались вопромы, то задавайте их мне по телефогу', '(068) 068-68-68','example@gamil.com']})
